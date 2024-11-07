0a31769ce36a755f76c30ffd603db0b53e688b8d
sirwan <sirwan.farajpanah79@gmail.com>
2024-11-07 18:06:12 +0330

    chore: move userAuthMiddleware.js from ./middleware to ./middleware/user

---
+++ src/middlewares/user/userAuthMiddleware.js
@@ -0,0 +1,49 @@
+// ./src/middlewares/user/userAuthMiddleware.js
+const jwt = require('jsonwebtoken');
+const apiError = require('../../utils/apiError');
+
+
+const preventSignupIfLoggedIn = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+        if (token) {
+            jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+                if (err) {
+                    res.clearCookie("jwt");
+                    next();
+                } else {
+                    return res.status(401).json({ message: `Already Logged In.` });
+                }
+            });
+        } else {
+            next();
+        }
+    } catch (error) {
+        next(error instanceof apiError ? error : new apiError(500, error.message));
+    }
+};
+
+const verifyToken = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+        if(token) {
+            jwt.verify(token, process.env.JWT_SECRET, (err) => {
+                if(err) {
+                    return res.status(401).json({ message: err.message });
+                } else {
+                    next();
+                }
+            })
+        } else {
+            return res.status(401).json({ message: `You Are Not Logged In.` });
+        }
+    } catch (error) {
+        next(error instanceof apiError ? error : new apiError(500, error.message));
+    }
+}
+
+
+module.exports = {
+    preventSignupIfLoggedIn,
+    verifyToken
+}
