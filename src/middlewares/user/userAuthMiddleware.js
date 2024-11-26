c46ca713a765233012eff240a91dccfab24e0291
sirwan <sirwan.farajpanah79@gmail.com>
2024-11-26 18:06:10 +0330

    feat(user-auth): handle token verification errors properly in userAuthMiddleware.js
    - Updated the `preventSignupIfLoggedIn` function in `userAuthMiddleware.js`.
    - Modified the error handling logic to ensure that, instead of proceeding to the next middleware on token verification failure, it responds to the client with an appropriate error message.
    - This change improves security and provides better feedback to users when token verification fails.

--- src/middlewares/user/userAuthMiddleware.js
+++ src/middlewares/user/userAuthMiddleware.js
@@ -9,8 +9,9 @@ const preventSignupIfLoggedIn = async (req, res, next) => {
         if (token) {
             jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
                 if (err) {
+                    req.body = {};
                     res.clearCookie("jwt");
-                    next();
+                    return res.status(401).json({ message: `Invalid token. Please log in again.` });
                 } else {
                     return res.status(401).json({ message: `Already Logged In.` });
                 }
