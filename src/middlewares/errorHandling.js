567da80c3e0cd2fddf8fc415f830ca8c6a746f38
sirwan <sirwan.farajpanah79@gmail.com>
2024-12-03 19:37:07 +0330

    fix(error-handling): resolve bugs in errorHandling.js

--- src/middlewares/errorHandling.js
+++ src/middlewares/errorHandling.js
@@ -4,7 +4,7 @@ const apiError = require('../utils/apiError');
 
 const errorHandler = async (error, req, res, next) => {
     if (error instanceof apiError) {
-        return res.status(error.statusCode).json({ message: error.details[0].message });
+        return res.status(error.statusCode).json({ message: error.message });
     }
     return res.status(500).json({ message: 'Internal Server Error.' });
 };
